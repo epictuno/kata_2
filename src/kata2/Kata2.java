da4000ebd74c3e88dba1e698e95c7184d5165a11
tanay <tanay@DESKTOP-J2I6D30>
2023-01-01 18:24:32 +0000

    V5 funciono igual antes

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,8 +9,8 @@ public class Kata2 {
     public static void main(String[] args) {
         String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histo=new Histogram(data);
-        Map<Integer,Integer> histogr = histo.getHistogram();
-        Iterator<Map.Entry<Integer,Integer>> iterador=histogr.entrySet().iterator();
+        Map<String,Integer> histogr = histo.getHistogram();
+        Iterator<Map.Entry<String,Integer>> iterador=histogr.entrySet().iterator();
         while(iterador.hasNext()){
             Map.Entry entry = (Map.Entry) iterador.next();
             String key = (String)entry.getKey();
